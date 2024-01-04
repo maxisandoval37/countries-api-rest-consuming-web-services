19572d217bd2a505167615ebd7af05b0044e4f35
Maxi Sandoval <maxisandoval98@gmail.com>
2024-01-04 20:17:04 -0300

    consumo de servicio soap

---
+++ src/main/java/dev/maxisandoval/countriesapirestconsumingwebservices/webservice/soap/PaisSoapClient.java
@@ -0,0 +1,27 @@
+package dev.maxisandoval.countriesapirestconsumingwebservices.webservice.soap;
+
+import localhost.ws.ObtenerPaisRequest;
+import localhost.ws.ObtenerPaisResponse;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.slf4j.Logger;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+public class PaisSoapClient extends WebServiceGatewaySupport {
+
+    private static final Logger log = LoggerFactory.getLogger(PaisSoapClient.class);
+    private static final String baseUrl = "http://localhost:8081/ws";
+
+    public ObtenerPaisResponse obtenerPais (String pais) {
+        ObtenerPaisRequest request = new ObtenerPaisRequest();
+        request.setName(pais);
+
+        log.info("Obteniendo info del pais: " + pais);
+
+        ObtenerPaisResponse response = (ObtenerPaisResponse) getWebServiceTemplate()
+                .marshalSendAndReceive(baseUrl+"/paises", request,
+                        new SoapActionCallback(baseUrl+"/obtenerPaisRequest"));
+
+        return  response;
+    }
+}
---
+++ src/main/java/dev/maxisandoval/countriesapirestconsumingwebservices/webservice/soap/PaisSoapConfiguration.java
@@ -0,0 +1,27 @@
+package dev.maxisandoval.countriesapirestconsumingwebservices.webservice.soap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class PaisSoapConfiguration {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        //Debe llamarse igual que el packageName del pom
+        marshaller.setContextPath("localhost.ws");
+        return marshaller;
+    }
+
+    @Bean
+    public PaisSoapClient paisSoapClient (Jaxb2Marshaller marshaller) {
+        PaisSoapClient client = new PaisSoapClient();
+        client.setDefaultUri("http://localhost:8081/ws");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+
+        return client;
+    }
+}
