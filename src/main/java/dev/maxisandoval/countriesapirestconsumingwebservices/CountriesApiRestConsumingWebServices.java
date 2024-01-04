ba76e5be6ad66ef47ad21799ce4f7cff02d7fcbf
Maxi Sandoval <maxisandoval98@gmail.com>
2024-01-04 19:36:29 -0300

    first commit

---
+++ src/main/java/dev/maxisandoval/countriesapirestconsumingwebservices/CountriesApiRestConsumingWebServices.java
@@ -0,0 +1,13 @@
+package dev.maxisandoval.countriesapirestconsumingwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesApiRestConsumingWebServices {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesApiRestConsumingWebServices.class, args);
+	}
+
+}
