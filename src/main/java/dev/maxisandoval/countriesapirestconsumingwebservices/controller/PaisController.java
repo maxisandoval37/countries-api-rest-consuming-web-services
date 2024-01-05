29b3e570107912d63a49ed7e0cefc3af798339e6
Maxi Sandoval <maxisandoval98@gmail.com>
2024-01-04 22:01:51 -0300

    agregado controlador con datos del soap y de una api rest

--- src/main/java/dev/maxisandoval/countriesapirestconsumingwebservices/controller/PaisController.java
+++ src/main/java/dev/maxisandoval/countriesapirestconsumingwebservices/controller/PaisController.java
@@ -1,8 +1,12 @@
 package dev.maxisandoval.countriesapirestconsumingwebservices.controller;
 
+import dev.maxisandoval.countriesapirestconsumingwebservices.model.PaisDTO;
+import dev.maxisandoval.countriesapirestconsumingwebservices.model.apiResponse.CountryData;
+import dev.maxisandoval.countriesapirestconsumingwebservices.webservice.apirest.PaisRestService;
 import dev.maxisandoval.countriesapirestconsumingwebservices.webservice.soap.PaisSoapClient;
 import localhost.ws.ObtenerPaisResponse;
-import localhost.ws.Pais;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,23 +16,65 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaisController {
 
     private final PaisSoapClient paisSoapClient;
+    private final PaisRestService paisRestService;
+    private static final Logger log = LoggerFactory.getLogger(PaisController.class);
 
     @Autowired
-    public PaisController (PaisSoapClient paisSoapClient) {
+    public PaisController (PaisSoapClient paisSoapClient, PaisRestService paisRestService) {
         this.paisSoapClient = paisSoapClient;
+        this.paisRestService = paisRestService;
     }
 
-    @GetMapping("obtenerPais")
-    public Pais obtenerPais(@RequestParam String nombre){
-        ObtenerPaisResponse response = paisSoapClient.obtenerPais(nombre);
+    @GetMapping("/obtenerPais")
+    public PaisDTO obtenerPais(@RequestParam String nombre) {
+        ObtenerPaisResponse soapResponse = obtenerDatosSOAP(nombre);
+        CountryData  apiResponse = obtenerDatosAPI(nombre);
 
-        Pais pais = new Pais();
-        pais.setNombre(response.getPais().getNombre());
-        pais.setCapital(response.getPais().getCapital());
-        pais.setMoneda(response.getPais().getMoneda());
-        pais.setPoblacion(response.getPais().getPoblacion());
-        pais.setBandera(response.getPais().getBandera());
+        return construirPaisDTO(soapResponse, apiResponse);
+    }
 
-        return pais;
+    private ObtenerPaisResponse obtenerDatosSOAP(String nombre) {
+        try {
+            return paisSoapClient.obtenerPais(nombre);
+        }
+        catch (Exception e) {
+            log.error("No se obtuvo info del SOAP");
+            return null;
+        }
+    }
+
+    private CountryData obtenerDatosAPI(String nombre) {
+        try {
+            return paisRestService.obtenerPaisInfoApi(nombre);
+        }
+        catch (Exception e) {
+            log.error("No se obtuvo info de la API");
+            return null;
+        }
+    }
+
+    private PaisDTO construirPaisDTO(ObtenerPaisResponse soapResponse, CountryData apiResponse){
+        PaisDTO paisDTO = new PaisDTO();
+
+        if (soapResponse != null) {
+            paisDTO.setNombre(soapResponse.getPais().getNombre());
+            paisDTO.setCapital(soapResponse.getPais().getCapital());
+            paisDTO.setMoneda(soapResponse.getPais().getMoneda());
+            paisDTO.setPoblacion(soapResponse.getPais().getPoblacion());
+            paisDTO.setBandera(soapResponse.getPais().getBandera());
+        }
+        else {
+            log.error("La respuesta del SOAP es nula");
+        }
+
+        if (apiResponse != null) {
+            paisDTO.setLenguajes(apiResponse.getLanguages());
+            paisDTO.setMapas(apiResponse.getMaps());
+        }
+        else {
+            log.error("La respuesta de la API es nula");
+        }
+
+        return paisDTO;
     }
 }
